f8587e7f023cbae9b8f9aebd90729349fa7d251d
Shamnas abdulla <mkshamnas278@gmail.com>
2024-08-10 18:51:04 +0530

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -135,3 +135,16 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+    50: "critical",
+}
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = True
