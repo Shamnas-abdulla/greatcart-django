27d739e8ad64ab04ff79595494dc862028fd5849
Shamnas abdulla <143501308+Shamnas-abdulla@users.noreply.github.com>
2024-10-28 00:04:28 +0530

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -161,8 +161,8 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shamnasabdulla2002@gmail.com'
-EMAIL_HOST_PASSWORD = 'fmio ilzd zzjk xqvb'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 
